b5a7dbad784ddd979cadbf4782feca1043277611
Pedro Pichardo <pichardodev@gmail.com>
2024-06-14 03:39:45 -0400

    built calculator

---
+++ JS101/lesson_2/calculator.js
@@ -0,0 +1,34 @@
+const readline = require('readline-sync');
+
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+prompt('Welcome to Calculator!');
+
+prompt('Input the first number');
+let number1 = readline.question();
+
+prompt('Input the second number');
+let number2 = readline.question();
+
+prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+let operation = readline.question();
+
+let output;
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
+    output = Number(number1) / Number(number2);
+    break;
+}
+
+console.log(`The result is: ${output}`);
