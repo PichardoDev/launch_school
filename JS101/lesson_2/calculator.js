04fc69c5a2ef65d42018ed0a1c20486c7e645d4f
Pedro Pichardo <pichardodev@gmail.com>
2024-06-16 04:23:12 -0400

    FIX CALCULATOR BUG AND ESLINT COMPLIANCE

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -1,70 +1,78 @@
 const jsonMessage = require('/home/ubuntu/launch_school/JS101/lesson_2/calculator_messages.json');
 const readline = require('readline-sync');
 let language = 'en';
-  
+
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
 function selectLanguage() {
 
-  availableLanguages = ['en', 'pt', 'de', 'jp', 'es'];
+  const AVAILABLE_LANGUAGES = ['en', 'pt', 'de', 'jp', 'es'];
   let code = readline.question(prompt(jsonMessage.language));
-  while(!availableLanguages.includes(code)) {
+  while (!AVAILABLE_LANGUAGES.includes(code)) {
     prompt('Sorry not a valid language, try again!\n');
     code = readline.question(prompt(jsonMessage.language));
   }
-    return code;
+  return code;
 }
 
-function getNumber(ordinalNumber) {
+function getNumber(ordiNumber) {
 
-  let numberStr = readline.question(prompt(jsonMessage[language][ordinalNumber]));
+  let numberStr = readline.question(prompt(jsonMessage[language][ordiNumber]));
   let numberNum = Number(numberStr);
-  while(!numberStr || Number.isNaN(numberNum)){
-    number = readline.question(prompt('Sorry not a valid number, try again!'));
+  while (!numberStr || Number.isNaN(numberNum)) {
+    numberStr = readline.question(prompt('Sorry not a valid number, try again!'));
     numberNum = Number(numberStr);
   }
   return numberNum;
+}
+
+function performOperation(operation, number1, number2) {
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = number1 + number2;
+      break;
+    case '2':
+      output = number1 - number2;
+      break;
+    case '3':
+      output = number1 * number2;
+      break;
+    case '4':
+      output = number1 / number2;
+      break;
   }
+  return output;
+}
 
-function performOperation() {
+function selectOperation() {
 
-  let operations = ['1', '2', '3', '4'];
-  let number1 = getNumber('first_number');
-  let number2 = getNumber('second_number');
+  let OPERATIONS = ['1', '2', '3', '4'];
   let operation = readline.question(prompt(jsonMessage[language].operation));
-  console.log(operation);
-  while (!operations.includes(operation)){
+  while (!OPERATIONS.includes(operation)) {
     prompt('Sorry not a valid mathematic operation');
     operation = readline.question(prompt(jsonMessage[language].operation));
   }
-  switch (operation) {
-    case '1':
-      return number1 + number2;
-    case '2':
-      return number1 - number2;
-    case '3':
-      return number1 * number2;
-    case '4':
-      return number1 / number2;
-  }
+  return operation;
 }
-  
-function continueCalculation () {
+
+function continueCalculation() {
   prompt("Would like to perform another calculation? press Y (Yes) or N (No)");
-  return readline.question() == 'y' ? true : false;
+  return readline.question();
 }
 
-function runCalculator (){
+function runCalculator() {
 
-prompt(jsonMessage[language].greeting);
-language = selectLanguage();
-console.log(`${jsonMessage[language].result} ${performOperation(jsonMessage[language].operation)}`);
+  prompt(jsonMessage[language].greeting);
+  language = selectLanguage();
+  console.log(`${jsonMessage[language].result} ${performOperation(selectOperation(), getNumber('first_number'), getNumber('second_number'))}`);
 
 }
 
 do {
   runCalculator();
-}while (continueCalculation());
+} while (continueCalculation());
 
