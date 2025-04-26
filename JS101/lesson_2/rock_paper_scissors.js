17bb7ac02cbd91efc28cf51501d2ff6420682656
Pedro Pichardo <pichardodev@gmail.com>
2025-04-26 17:43:12 -0400

    ADD TWENTY ONE - LS & MY

--- JS101/lesson_2/rock_paper_scissors.js
+++ JS101/lesson_2/rock_paper_scissors.js
@@ -1,200 +1,83 @@
 const readline = require('readline-sync');
-const MESSAGE = require('./rock_paper_scissors_lizard_spock_messages.json');
-const PLAYED = { games : 0 };
-const CONTENDERS = [
-  {
-    name : 'player',
-    score : 0
-  },
-  {
-    name : 'computer',
-    score : 0
-  }
-];
+//const MESSAGE = require('./rock_paper_scissors.json');
 const VALID_CHOICES = [
-  { name : "rock",
-    alias : "r"
-  },
-  { name : "paper",
-    alias : "p"
-  },
-  { name : "scissors",
-    alias : "s"
-  },
-  { name : "lizard",
-    alias : "l"
-  },
-  { name : "spock",
-    alias : "k"
-  }
-];
+                        ['r', 'rock'],
+                        ['p', 'paper'],
+                        ['s', 'scissors'],
+                        ['l', 'lizard'], 
+                        ['k', 'spock']
+                      ]
+const CHOICE = {  
+  player : undefined,
+  computer : undefined
+}
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function convertAliasToName(alias) {
-  let convertedAlias = "";
-  VALID_CHOICES.forEach(option => {
-    if (option.alias === alias) {
-      convertedAlias = option.name;
-    }
-  });
-  return convertedAlias;
+function validateChose() {
+
 }
 
-function decideWinner(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}`);
+function getPlayerChose() {
 
-  if (choice === computerChoice) {
-    return "tie";
-  } else if (
-    (choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) ||
-    (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
-    (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) ||
-    (choice === 'lizard' && (computerChoice === 'paper' || computerChoice === 'spock')) ||
-    (choice === 'spock' && (computerChoice === 'rock' || computerChoice === 'scissors'))
-  ) {
-    return 'player';
-  } else {
-    return 'computer';
+}
+
+function getComputerChose() {
+
+}
+
+function displayWinner(winner) {
+
+  prompt(`You chose ${CHOICE.player}, computer chose ${CHOICE.computer}`);
+
+}
+
+function continueGame() {
+
+  prompt('Do you want to play again (y/n)?');
+  let answer = readline.question().toLowerCase();
+  while (answer[0] !== 'n' && answer[0] !== 'y') {
+    prompt('Please enter "y" or "n".');
+    answer = readline.question().toLowerCase();
   }
-}
 
-function formatValidChoices () {
-  let formatedArray = VALID_CHOICES.map((element) => `${element.name} (${element.alias})`);
-  return formatedArray;
-}
-
-function decideBestOfFive() {
-  let winner = 'no winner yet';
-  CONTENDERS.forEach((contender) => {
-    if (contender.score >= 3) {
-      winner = contender.name;
-    }
-  });
-  return winner;
-}
-
-function resetPlayedGames() {
-  PLAYED.games = 0;
-}
-
-function incrementPlayedGames() {
-  PLAYED.games += 1;
-}
-
-function resetScores() {
-  CONTENDERS.forEach(contender => {
-    contender.score = 0;
-  });
-}
-
-function updateScore(winner) {
-  CONTENDERS.forEach(contender => {
-    if (contender.name === winner) {
-      contender.score += 1;
-    }
-  });
-}
-
-function validateChoice (choice) {
-  let validOption = false;
-  VALID_CHOICES.forEach(
-    (option) => {
-      if (option.name === choice) {
-        validOption = choice;
-      } else if (option.alias === choice) {
-        validOption = convertAliasToName(choice);
-      }
-    });
-  
-  return validOption;
-}
-
-function playerSelect() {
-
-  prompt(`${MESSAGE.choice} ${formatValidChoices().join(', ')}`);
-  let choice = "";
-  choice = readline.question();
-  let validChoice = validateChoice(choice);
-  if (validChoice === false) {
-    prompt(`${MESSAGE.invalid}`);
-    return playerSelect();
-  }
-  return validChoice;
-}
-
-function computerSelect() {
-  let randomIndex = Math.round(Math.random() * (VALID_CHOICES.length - 1));
-  return VALID_CHOICES[randomIndex].name;
-}
-
-function playGame () {
-
-  let playerChoice = playerSelect();
-  let computerChoice = computerSelect();
-  let result = decideWinner(playerChoice, computerChoice);
-  displayGameResult(result);
-  updateScore(result);
-  incrementPlayedGames();
-  return result;
-}
-
-function displayGameResult(result) {
-  if (result === 'tie') {
-    prompt(`${MESSAGE.game_tie}`);
-  } else {
-    prompt(`${result} ${MESSAGE.game_winner}`);
-  }
-}
-
-function displayMatchWinner(winner) {
-  prompt(`${winner} ${MESSAGE.match_winner}`);
-}
-
-function validateRepeat(answer) {
-
-  if (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt(`${MESSAGE.invalid}`);
-    repeat();
-  } else if (answer === 'y') {
+  if (answer[0] == 'y')
     return true;
-  } else {
+  else
     return false;
   }
+
+function determineWinner(choice, computerChoice) {
+
+  if (choice === computerChoice) {
+    return 't';
+  } else if ((choice === 'rock' && (computerChoice === 'scissors' || computerChoice === 'lizard')) || 
+             (choice === 'paper' && (computerChoice === 'rock' || computerChoice === 'spock')) || 
+             (choice === 'scissors' && (computerChoice === 'paper' || computerChoice === 'lizard')) ||
+             (choice === 'lizard' && (computerChoice === 'paper' || computerChoice === 'spock')) ||
+             (choice === 'spock' && (computerChoice === 'rock' ||  computerChoice === 'scissors'))) {
+    return 'p';
+  } else {
+    return 'c';
+  } 
 }
 
-function repeat() {
+do {
+  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
+  let choice = readline.question();
 
-  prompt(`${MESSAGE.again}`);
-  let answer = readline.question().toLowerCase();
-  validateRepeat(answer);
-  return answer;
+  while (!VALID_CHOICES.includes(choice)) {
+    prompt("That's not a valid choice");
+    choice = readline.question();
+  }
+  //let randomIndex = Math.ceil(Math.random() * VALID_CHOICES.length) - 1;
+  let randomIndex = Math.round(Math.random() * VALID_CHOICES.length - 1);
+  let computerChoice = VALID_CHOICES[randomIndex];
 
-}
+  prompt(displayWinner(choice, computerChoice));
 
-function playMatch() {
 
-  do {
-    resetPlayedGames();
-    resetScores();
-    let matchWinner = 'no winner yet';
-    do {
-      playGame();
-      if (PLAYED.games >= 3) {
-        matchWinner = decideBestOfFive();
-      }
-    } while (matchWinner === 'no winner yet');
-    displayMatchWinner(matchWinner);
+} while (repeat());
 
-  } while (repeat() === 'y');
-}
-
-function playSession() {
-
-  playMatch();
-
-}
-
-playSession();
